ee17633f9113e4f2d08ce1b4f6fccc47c87036f3
CodeBurner <codeburner0@gmail.com>
2023-05-16 16:48:50 +0530

    ExternalCSS

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ import React from 'react'
 // import DidUnmount from './Components/Life Cycle Methods/class methods/DidUnmount'
 // import UseState from './Components/Hooks/UseState'
 // import UseEffect from './Components/Hooks/UseEffect'
-import Internal from './Components/Styling In react/Internal'
+// import Internal from './Components/Styling In react/Internal'
+import ExternalCSS from './Components/Styling In react/ExternalCSS'
 
 function App() {
   return (
     <div>
-      <Internal/>
+      <ExternalCSS/>
     </div>
   )
 }
---
+++ src/Components/Styling In react/ExternalCSS.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './External.css'
+function ExternalCSS() {
+  return (
+    <div>
+      <h1>Hello! Ankit Anand</h1>
+    </div>
+  )
+}
+
+export default ExternalCSS
