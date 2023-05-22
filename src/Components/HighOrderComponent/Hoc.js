c471bd234ffc3ef1914d86757bb034fcc214dde5
CodeBurner <codeburner0@gmail.com>
2023-05-22 21:12:59 +0530

    Hoc updated

--- src/Components/HighOrderComponent/Hoc.js
+++ src/Components/HighOrderComponent/Hoc.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function HocRED(props) {
   return (
-    <h1 style={{backgroundColor:'red',color:'gray',width:'100px'}}><props.CounterFunction/>Red</h1>
+    <h1 style={{backgroundColor:'red',color:'orange',width:'100px'}}><props.CounterFunction/>Red</h1>
   )
 }
 function HocGREEN(props) {
   return (
-    <h1 style={{backgroundColor:'green',color:'gray',width:'100px'}}><props.CounterFunction/>Green</h1>
+    <h1 style={{backgroundColor:'green',color:'brown',width:'100px'}}><props.CounterFunction/>Green</h1>
   )
 }
 export {HocRED,HocGREEN};
