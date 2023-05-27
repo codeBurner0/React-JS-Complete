0a19f3d4f1d6386699f2d4e006d386f9c3a26b3d
CodeBurner <codeburner0@gmail.com>
2023-05-27 14:38:11 +0530

    Page 404

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -3,13 +3,15 @@ import {Routes,Route} from 'react-router-dom'
 import Nav from './Components/Nav'
 import Home from './Components/Home'
 import About from './Components/About'
+import PageNotFound from './Components/PageNotFound'
 function Routing() {
   return (
     <div>
-      < Nav/>
+      <Nav/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/About" element={<About/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </div>
   )
